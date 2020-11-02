17ede1cda00d99d6be26c608adbf4c0823f3a8d1
Lavinia Bratis <lavinia.adriana.bratis@sap.com>
2020-11-02 17:47:23 +0200

    change

--- app/src/main/java/com/example/pimpmywed/ui/MainActivity.kt
+++ app/src/main/java/com/example/pimpmywed/ui/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         binding.lifecycleOwner = this
 
         AndroidThreeTen.init(this)
